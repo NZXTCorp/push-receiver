862e93d39d34bdbfc640731c17dee413e91053d7
Eduardo Guzman Lau Len <eduardo.laulen@nzxt.com>
2022-11-23 15:06:56 -0500

    fix: solve test timeout issues

--- test/notification.test.js
+++ test/notification.test.js
@@ -1,6 +1,8 @@
-const axios = require('axios')
+const axios = require('axios');
 const { SENDER_ID, SERVER_KEY } = require('./keys');
 const { register, listen } = require('../src/index');
+
+const TIMEOUT = 500000;
 
 const NOTIFICATIONS = {
   SIMPLE : { title : 'Hello world ', body : 'Test' },
@@ -9,51 +11,65 @@ const NOTIFICATIONS = {
 
 let credentials;
 let client;
-describe('Parser', function() {
-  beforeEach(async function() {
+describe('Parser', function () {
+  beforeEach(async function () {
     credentials = await register(SENDER_ID);
   });
 
-  afterEach(async function() {
-    client.destroy();
+  afterEach(async function () {
+    if (client) {
+      client.destroy();
+    }
     credentials = null;
   });
 
-  it('should receive a simple notification', async function() {
-    await send(NOTIFICATIONS.SIMPLE);
-    const notifications = await receive(1);
-    expect(notifications.length).toEqual(1);
-    expect(notifications[0].notification.notification).toEqual(
-      NOTIFICATIONS.SIMPLE
-    );
-  });
+  it(
+    'should receive a simple notification',
+    async function () {
+      await send(NOTIFICATIONS.SIMPLE);
+      const notifications = await receive(1);
+      expect(notifications.length).toEqual(1);
+      expect(notifications[0].notification.notification).toEqual(
+        NOTIFICATIONS.SIMPLE
+      );
+    },
+    TIMEOUT
+  );
 
-  it('should receive a large notification', async function() {
-    await send(NOTIFICATIONS.LARGE);
-    const notifications = await receive(1);
-    expect(notifications.length).toEqual(1);
-    expect(notifications[0].notification.notification).toEqual(
-      NOTIFICATIONS.LARGE
-    );
-  });
+  it(
+    'should receive a large notification',
+    async function () {
+      await send(NOTIFICATIONS.LARGE);
+      const notifications = await receive(1);
+      expect(notifications.length).toEqual(1);
+      expect(notifications[0].notification.notification).toEqual(
+        NOTIFICATIONS.LARGE
+      );
+    },
+    TIMEOUT
+  );
 
-  it('should receive multiple notifications', async function() {
-    await send(NOTIFICATIONS.SIMPLE);
-    await send(NOTIFICATIONS.LARGE);
-    await send(NOTIFICATIONS.SIMPLE);
+  it(
+    'should receive multiple notifications',
+    async function () {
+      await send(NOTIFICATIONS.SIMPLE);
+      await send(NOTIFICATIONS.LARGE);
+      await send(NOTIFICATIONS.SIMPLE);
 
-    const notifications = await receive(3);
-    expect(notifications.length).toEqual(3);
-    expect(notifications[0].notification.notification).toEqual(
-      NOTIFICATIONS.SIMPLE
-    );
-    expect(notifications[1].notification.notification).toEqual(
-      NOTIFICATIONS.LARGE
-    );
-    expect(notifications[2].notification.notification).toEqual(
-      NOTIFICATIONS.SIMPLE
-    );
-  });
+      const notifications = await receive(3);
+      expect(notifications.length).toEqual(3);
+      expect(notifications[0].notification.notification).toEqual(
+        NOTIFICATIONS.SIMPLE
+      );
+      expect(notifications[1].notification.notification).toEqual(
+        NOTIFICATIONS.LARGE
+      );
+      expect(notifications[2].notification.notification).toEqual(
+        NOTIFICATIONS.SIMPLE
+      );
+    },
+    TIMEOUT
+  );
 });
 
 async function send(notification) {
@@ -78,14 +94,14 @@ async function send(notification) {
 
 async function receive(n) {
   const received = [];
-  return new Promise(async resolve => {
-    const onNotification = notification => {
+  return new Promise((resolve) => {
+    const onNotification = (notification) => {
       received.push(notification);
       if (received.length === n) {
         resolve(received);
       }
     };
     credentials.persistentIds = [];
-    client = await listen(credentials, onNotification);
+    listen(credentials, onNotification).then((result) => (client = result));
   });
 }
